94cd784a3eac26a9bd80255fa77369074b3fff46
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-12 11:38:36 -0400

    added result

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState }from 'react';
 import Search from './components/Search'
+import Results from './components/Results'
 import axios from 'axios'
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           });
         });
     }
+   
   }
 
   const handleInput = (event) => {
@@ -37,6 +39,7 @@ function App() {
       </header>
       <main>
         <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results} />
       </main>
     </div>
   );
