6d0f16fdfac639baafe8bd0af9a2f96f36753bc5
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-11 23:22:11 -0400

    added handleInput function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { useState }from 'react';
 import Search from './components/Search'
 
 function App() {
+  const [state, setState] = useState({
+    movie:"",
+    resule:[],
+    selected:{}
+  });
   const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=92c99205";
-
-  const handleInput = (event){
-    let vaule = event.traget.vaule
+  
+  const handleInput = (event) => {
+    let movie = event.traget.vaule
+    setState(preveState => {
+      return {...preveState, movie: movie}
+    })
   }
+  
   return (
     <div className="App">
       <header>
