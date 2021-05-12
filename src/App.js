60fc2191e5c6a8e7c18e9b613014c93353b53bf2
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-12 11:10:01 -0400

    added search keypress

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React, { useState }from 'react';
 import Search from './components/Search'
+import axios from 'axios'
 
 function App() {
   const [state, setState] = useState({
     movie:"",
-    resule:[],
+    results:[],
     selected:{}
   });
   const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=92c99205";
   
+  const search = (event) => {
+    if (event.key === "Enter"){
+      axios(apiurl + "&s=" + state.movie).then(({data}) => {
+          let results = data.Search;
+
+          setState(preveState => {
+            return {...preveState, results: results}
+          });
+        });
+    }
+  }
+
   const handleInput = (event) => {
     let movie = event.target.value
     setState(preveState => {
       return {...preveState, movie: movie}
     });
-    console.log(state.movie)
+    
   }
   
   return (
     <div className="App">
       <header>
-       <h1>Movie App</h1>
+       <h1>Movie Database</h1>
       </header>
       <main>
-        <Search handleInput={handleInput} />
+        <Search handleInput={handleInput} search={search}/>
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function Search ({ handleInput }) {
+function Search ({ handleInput, search }) {
     return (
         <div>
             <section className='searchbox-wrap'>
                 <input 
                     onChange={handleInput} 
+                    onKeyPress={search}
                     className='searchbox' 
                     type='text' 
                     placeholder='Search Your Movie'
